d1421439c5925253335e3419001a280068774731
Jagman0477 <jags0477@gmail.com>
2023-10-25 16:27:13 +0530

    Eureka work

--- src/main/java/com/jagman/HealthInsuranceApplication.java
+++ src/main/java/com/jagman/HealthInsuranceApplication.java
@@ -2,9 +2,10 @@ package com.jagman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HealthInsuranceApplication {
 
 	public static void main(String[] args) {
